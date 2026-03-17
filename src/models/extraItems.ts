9386c1e61e9a7372dfa9e2d344740f1708714e6f
loranpartu <lpartu@st.ug.edu.gh>
2026-03-17 19:02:44 +0000

    added extraitems schema

---
+++ src/models/extraItems.ts
@@ -0,0 +1,16 @@
+import { Schema } from "mongoose";
+
+export const ExtraItemSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+});
